6e738ce7ffec13c286e0efc68570b4bb7b2d8e50
Louis Popi <theh2o64@gmail.com>
2015-12-06 18:11:15 +0100

    jalebi: Remove project-path

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -54,7 +54,7 @@ LOCAL_CFLAGS += -DUSE_KK_CODE
 endif
 
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
-#LOCAL_C_INCLUDES += hardware/qcom/display/msm8974/libgralloc
+#LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 #else
 LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 #endif
