6af44291da9dad7e543bcc443054ef218885b5b5
Louis Popi <theh2o64@gmail.com>
2016-07-26 16:56:07 +0200

    jalebi: Update loc_api to LA.BR.1.2.7-02810-8x16.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Qualcomm support
-BOARD_USES_QCOM_HARDWARE := false
+BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/yu/jalebi/ramdisk/fstab.qcom
