3514020bf15b6c1ce0f2e1fec9d6321cc2673477
Louis Popi <theh2o64@gmail.com>
2016-12-20 12:54:24 +0100

    jalebi: Add gapps configuration (for internal testing only)

--- device.mk
+++ device.mk
@@ -18,6 +18,11 @@ $(call inherit-product-if-exists, vendor/camera/camera.mk)
 $(call inherit-product-if-exists, vendor/volte/volte.mk)
 $(call inherit-product-if-exists, vendor/volte/jalebi/jalebi-vendor.mk)
 
+# Gapps
+$(call inherit-product-if-exists, vendor/google/build/opengapps-packages.mk)
+GAPPS_VARIANT := mini
+GAPPS_FORCE_MATCHING_DPI := true
+
 # OpenGLES
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
