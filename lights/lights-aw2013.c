3c9be0efdf88699875118fa886ea7518c95a96d6
Louis Popi <theh2o64@gmail.com>
2017-01-22 07:55:14 +0100

    jalebi: Import liblight from hardware/cyanogen

--- lights/lights-aw2013.c
+++ lights/lights-aw2013.c
@@ -22,6 +22,7 @@
 #include <cutils/log.h>
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
@@ -510,7 +511,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "YU Lights Module",
+    .name = "Lights Module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
