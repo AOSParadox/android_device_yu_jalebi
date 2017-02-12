42b331c1150c2c072cf3a7d5c147068cf7b62f92
Louis Popi <theh2o64@gmail.com>
2017-02-12 19:26:16 +0100

    jalebi: Call mkbootimg args after their definition
     * lol

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,13 +83,13 @@ ENABLE_CPUSETS := true
 BOARD_CUSTOM_BOOTIMG_MK := device/yu/jalebi/mkbootimg.mk
 TARGET_NOT_USE_GZIP_RECOVERY_RAMDISK := true
 BOARD_DTBTOOL_ARGS := -2
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_SOURCE := kernel/yu/msm8916
 KERNEL_DEFCONFIG := jalebi_defconfig
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 TARGET_USE_CM_RAMDISK := true
 
 # Partitions
